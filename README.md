aa8eacc3f84e357a33480546ecf8d2915171d43b
kyle <tikioph@gmail.com>
2025-05-17 21:29:41 -0700

    add an enabled setting.

--- README.md
+++ README.md
@@ -12,16 +12,9 @@ There is also a function to create a link in the current file, and then create t
 
 ## Extension Settings
 
->Todo add settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+* `rhyolite.enabled`: Enable/disable this extension. When disabled, the commands will not be available.
 
 ## Known Issues
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,58 @@ import * as vscode from 'vscode';
 import { createFileLink, updateIndexFile } from './commands';
 
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "rhyolite" is now active!');
 
-	// Register the createFile command
-	const createFileCommand = vscode.commands.registerCommand('rhyolite.rhyCreateFile', createFileLink);
+	let createFileCommand: vscode.Disposable | undefined;
+	let updateIndexLinksCommand: vscode.Disposable | undefined;
 
-	// Register the updateIndexLinks command
-	const updateIndexLinksCommand = vscode.commands.registerCommand('rhyolite.updateIndexLinks', updateIndexFile);
+	// Function to register commands
+	function registerCommands() {
+		// Get the configuration
+		const config = vscode.workspace.getConfiguration('rhyolite');
+		const isEnabled = config.get<boolean>('enabled', true);
 
-	context.subscriptions.push(createFileCommand, updateIndexLinksCommand);
+		// Dispose existing commands if they exist
+		if (createFileCommand) {
+			createFileCommand.dispose();
+			createFileCommand = undefined;
+		}
+		if (updateIndexLinksCommand) {
+			updateIndexLinksCommand.dispose();
+			updateIndexLinksCommand = undefined;
+		}
+
+		// Only register commands if the extension is enabled
+		if (isEnabled) {
+			// Register the createFile command
+			createFileCommand = vscode.commands.registerCommand('rhyolite.rhyCreateFile', createFileLink);
+
+			// Register the updateIndexLinks command
+			updateIndexLinksCommand = vscode.commands.registerCommand('rhyolite.updateIndexLinks', updateIndexFile);
+
+			// Add commands to subscriptions
+			if (createFileCommand) context.subscriptions.push(createFileCommand);
+			if (updateIndexLinksCommand) context.subscriptions.push(updateIndexLinksCommand);
+
+			console.log('Rhyolite commands registered - extension is enabled');
+		} else {
+			console.log('Rhyolite commands not registered - extension is disabled');
+		}
+	}
+
+	// Register commands initially
+	registerCommands();
+
+	// Listen for configuration changes
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeConfiguration(e => {
+			if (e.affectsConfiguration('rhyolite.enabled')) {
+				registerCommands();
+			}
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
